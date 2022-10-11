1bfc720dc1b87eb4f190ef897005fabac8dc6635
Gerard Keiser <gerardkeiser@gmail.com>
2022-10-11 14:46:49 -0500

    Authenticate user passes

--- tests/models_test.py
+++ tests/models_test.py
@@ -1,9 +1,15 @@
 import pytest
 
 from app.db import get_driver
-from app.models.user import register
+from app.models.user import register, authenticate
 
 from .fixtures import app
+
+test_user = {
+    "email": "test@test.com",
+    "password": "test_password",
+    "username": "test_user",
+}
 
 
 def test_make_new_user(app):
@@ -13,3 +19,12 @@ def test_make_new_user(app):
         user = register(driver, "test@test.com", "test_password", "test_user")
 
         assert user["email"] == "test@test.com"
+
+
+def test_get_user(app):
+    with app.app_context():
+        driver = get_driver()
+
+        user = authenticate(driver, test_user["email"], test_user["password"])
+
+        assert user["email"] == test_user["email"]
