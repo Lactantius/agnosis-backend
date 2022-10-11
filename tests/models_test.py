bf53a07d411563b37390060606805d4f2e665242
Gerard Keiser <gerardkeiser@gmail.com>
2022-10-11 14:53:30 -0500

    Use driver in a context to prevent deprecation warnings

--- tests/models_test.py
+++ tests/models_test.py
@@ -14,17 +14,19 @@ test_user = {
 
 def test_make_new_user(app):
     with app.app_context():
-        driver = get_driver()
+        with get_driver() as driver:
 
-        user = register(driver, "test@test.com", "test_password", "test_user")
+            user = register(
+                driver, test_user["email"], test_user["password"], test_user["username"]
+            )
 
-        assert user["email"] == "test@test.com"
+            assert user["email"] == test_user["email"]
 
 
 def test_get_user(app):
     with app.app_context():
-        driver = get_driver()
+        with get_driver() as driver:
 
-        user = authenticate(driver, test_user["email"], test_user["password"])
+            user = authenticate(driver, test_user["email"], test_user["password"])
 
-        assert user["email"] == test_user["email"]
+            assert user["email"] == test_user["email"]
